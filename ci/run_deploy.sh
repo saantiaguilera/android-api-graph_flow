c6381e4311bb499d7f8f733fb16f60160947257b
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-06-12 02:55:21 -0300

    Fix travis and create release manually

--- ci/run_deploy.sh
+++ ci/run_deploy.sh
@@ -8,9 +8,6 @@ PUBLISH_MODULES=(core conductor views fragments)
 EXIT_CODE=${PIPESTATUS[0]}
 
 if [ $EXIT_CODE -eq 0 ]; then
-	VERSION=$(grep "versionName = .*" gradle.properties | sed -E "s/versionName = (.*)/\1/g")
-	git tag -a v${VERSION} -m "v${VERSION}"
-
 	for module in ${PUBLISH_MODULES[@]}; do
 		./gradlew ${module}:bintrayUpload
 
@@ -19,5 +16,9 @@ if [ $EXIT_CODE -eq 0 ]; then
 		fi
     done
 fi
+
+VERSION=$(grep "versionName = .*" gradle.properties | sed -E "s/versionName = (.*)/\1/g")
+API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "%s","draft": false,"prerelease": false}' "$VERSION" "$VERSION" "$(cat CHANGELOG.md)")
+curl --data "$API_JSON" https://api.github.com/repos/saantiaguilera/android-api-graph_flow/releases/releases?access_token=$GITHUB_OAUTH_TOKEN
 
 exit 0
