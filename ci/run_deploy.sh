02586f0284e8e1bfef16df71227064623345dd53
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-06-12 03:11:40 -0300

    Fix build.gradle dep

--- ci/run_deploy.sh
+++ ci/run_deploy.sh
@@ -15,10 +15,10 @@ if [ $EXIT_CODE -eq 0 ]; then
 			exit 1
 		fi
     done
-fi
 
-VERSION=$(grep "versionName = .*" gradle.properties | sed -E "s/versionName = (.*)/\1/g")
-API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "%s","draft": false,"prerelease": false}' "$VERSION" "$VERSION" "$(cat CHANGELOG.md)")
-curl --data "$API_JSON" https://api.github.com/repos/saantiaguilera/android-api-graph_flow/releases/releases?access_token=$GITHUB_OAUTH_TOKEN
+	VERSION=$(grep "versionName = .*" gradle.properties | sed -E "s/versionName = (.*)/\1/g")
+	API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "","draft": false,"prerelease": false}' "$VERSION" "$VERSION")
+	curl --data "$API_JSON" https://api.github.com/repos/saantiaguilera/android-api-graph_flow/releases/releases?access_token=$GITHUB_OAUTH_TOKEN
+fi
 
 exit 0
