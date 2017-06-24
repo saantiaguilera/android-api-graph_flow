de217892bd2fdad2a3af93f9445c2c32bd87f7af
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-06-24 01:26:50 -0300

    Refactor publishing system

--- ci/run_deploy.sh
+++ ci/run_deploy.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-# Modules that will be published
-PUBLISH_MODULES=(core conductor views fragments)
-
 ./gradlew shouldPublish | sed -f "$STDOUT_FILTERS"
 
 EXIT_CODE=${PIPESTATUS[0]}
 
 if [ $EXIT_CODE -eq 0 ]; then
-	for module in ${PUBLISH_MODULES[@]}; do
-		./gradlew ${module}:bintrayUpload
+	./gradlew publishModules
 
-		node -e "setTimeout(null,30000)"; # 30 seconds delay
+	if [ $? -ne 0 ]; then
+		exit 1
+	fi
 
-		if [ $? -ne 0 ]; then
-			exit 1
-		fi
-    done
-
-	VERSION=$(grep "versionName = .*" gradle.properties | sed -E "s/versionName = (.*)/\1/g")
-	API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "For more information, please see the CHANGELOG.md of this tag","draft": false,"prerelease": false}' "$VERSION" "$VERSION")
+	VERSION=$(grep "libraryVersion = .*" gradle.properties | sed -E "s/libraryVersion = (.*)/\1/g")
+	API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "","draft": false,"prerelease": false}' "$VERSION" "$VERSION")
 	curl --data "$API_JSON" https://api.github.com/repos/saantiaguilera/android-api-graph_flow/releases?access_token=$GITHUB_OAUTH_TOKEN
+
+	exit $?
 fi
 
 exit 0
